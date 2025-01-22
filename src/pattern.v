a2f94be54bc60649449316716734b9a932644ba2
Jacob C <jacob.j.carlson03@gmail.com>
2025-01-21 17:29:25 -0800

    green column sprite

--- src/pattern.v
+++ src/pattern.v
@@ -6,10 +6,17 @@ module pattern(
     output [7:0] g,
     output [7:0] b
 );
-wire [7:0] p;
-assign p = x[7:0] ^ y[7:0];
-assign r = p;
-assign g = p;
-assign b = p;
+
+wire [9:0] spritePos;
+assign spritePos = 100;
+wire isSprite;
+assign isSprite = (x > spritePos) && (x < spritePos + 100);
+
+wire [7:0] checkerBg;
+assign checkerBg = x[7:0] ^ y[7:0];
+
+assign r = checkerBg;
+assign g = {8{isSprite}};
+assign b = checkerBg;
 
 endmodule
